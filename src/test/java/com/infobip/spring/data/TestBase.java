bc7e54d45e3167c34faa4d2f0c33de8924520b33
lpandzic <lovro.pandzic@infobip.com>
2019-12-16 08:38:15 +0100

    upgraded dependencies moved docker logic from travis to the app

--- src/test/java/com/infobip/spring/data/TestBase.java
+++ src/test/java/com/infobip/spring/data/TestBase.java
@@ -1,16 +1,20 @@
 package com.infobip.spring.data;
 
 import org.junit.After;
+import org.junit.Rule;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.testcontainers.containers.MSSQLServerContainer;
 
 import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = Main.class)
+@ContextConfiguration(initializers = LicenseAcceptedMSSQLServerContainer.class)
 public abstract class TestBase {
 
     @Autowired
