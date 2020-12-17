7e652f8e35e590bd1004a4f5e7b8cb324e7770d9
lpandzic <lovro.pandzic@infobip.com>
2020-12-17 14:16:25 +0100

    replaced common module with infobip-mssql-testcontainers-spring-boot-starter

--- infobip-spring-data-jdbc-querydsl/src/test/java/com/infobip/spring/data/jdbc/TestBase.java
+++ infobip-spring-data-jdbc-querydsl/src/test/java/com/infobip/spring/data/jdbc/TestBase.java
@@ -1,12 +1,10 @@
 package com.infobip.spring.data.jdbc;
 
-import com.infobip.spring.data.common.LicenseAcceptedMSSQLServerContainer;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestConstructor;
 
 import java.util.List;
@@ -16,7 +14,6 @@ import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 @TestInstance(PER_CLASS)
 @SpringBootTest(classes = Main.class)
-@ContextConfiguration(initializers = LicenseAcceptedMSSQLServerContainer.class)
 public abstract class TestBase {
 
     @Autowired
--- infobip-spring-data-jpa-querydsl/src/test/java/com/infobip/spring/data/jpa/TestBase.java
+++ infobip-spring-data-jpa-querydsl/src/test/java/com/infobip/spring/data/jpa/TestBase.java
@@ -1,12 +1,10 @@
 package com.infobip.spring.data.jpa;
 
-import com.infobip.spring.data.common.LicenseAcceptedMSSQLServerContainer;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestConstructor;
 
 import java.util.List;
@@ -16,7 +14,6 @@ import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 @TestInstance(PER_CLASS)
 @SpringBootTest(classes = Main.class)
-@ContextConfiguration(initializers = LicenseAcceptedMSSQLServerContainer.class)
 public abstract class TestBase {
 
     @Autowired
