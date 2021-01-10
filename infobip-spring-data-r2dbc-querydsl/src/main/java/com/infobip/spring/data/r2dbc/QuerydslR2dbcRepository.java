ee435b783f8c862cc21e65190b38af7f82560c94
lpandzic <lovro.pandzic@infobip.com>
2021-01-10 12:47:37 +0100

    added transactional to save in QuerydslR2dbcRepository

--- infobip-spring-data-r2dbc-querydsl/src/main/java/com/infobip/spring/data/r2dbc/QuerydslR2dbcRepository.java
+++ infobip-spring-data-r2dbc-querydsl/src/main/java/com/infobip/spring/data/r2dbc/QuerydslR2dbcRepository.java
@@ -3,6 +3,7 @@ package com.infobip.spring.data.r2dbc;
 import org.springframework.data.querydsl.ReactiveQuerydslPredicateExecutor;
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.reactive.ReactiveSortingRepository;
+import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Flux;
 
 import java.util.Arrays;
@@ -10,6 +11,7 @@ import java.util.Arrays;
 @NoRepositoryBean
 public interface QuerydslR2dbcRepository<T, ID> extends ReactiveSortingRepository<T, ID>, ReactiveQuerydslPredicateExecutor<T>, QuerydslR2dbcFragment<T> {
 
+    @Transactional
     default Flux<T> save(T... entities) {
         return this.saveAll(Arrays.asList(entities));
     }
