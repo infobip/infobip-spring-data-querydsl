ee4de0c4f3900ce60163cf07ea6ee3acfcd252e7
lpandzic <lovro.pandzic@infobip.com>
2021-01-10 07:45:24 +0100

    added QuerydslJdbcPredicateExecutorTest

---
+++ infobip-spring-data-jdbc-querydsl/src/test/java/com/infobip/spring/data/jdbc/QuerydslJdbcPredicateExecutorTest.java
@@ -0,0 +1,133 @@
+package com.infobip.spring.data.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.*;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+import java.util.Optional;
+
+import static com.infobip.spring.data.jdbc.QPerson.person;
+import static org.assertj.core.api.BDDAssertions.then;
+
+public class QuerydslJdbcPredicateExecutorTest extends TestBase {
+
+    private final PersonRepository repository;
+    private final QuerydslPredicateExecutor<Person> executor;
+
+    public QuerydslJdbcPredicateExecutorTest(PersonRepository repository,
+                                             QuerydslPredicateExecutor<Person> personRepository) {
+        this.repository = repository;
+        this.executor = personRepository;
+    }
+
+    @Test
+    void shouldFindOne() {
+        // given
+        Person johnDoe = givenSavedPerson("John", "Doe");
+        Person johnyRoe = givenSavedPerson("Johny", "Roe");
+
+        // when
+        Optional<Person> actual = executor.findOne(person.firstName.eq("John"));
+
+        then(actual).usingFieldByFieldValueComparator().contains(johnDoe);
+    }
+
+    @Test
+    void shouldFindAllWithPredicate() {
+        // given
+        Person johnDoe = givenSavedPerson("John", "Doe");
+        Person johnyRoe = givenSavedPerson("Johny", "Roe");
+        Person janeDoe = givenSavedPerson("Jane", "Doe");
+
+        // when
+        Iterable<Person> actual = executor.findAll(person.lastName.eq("Doe"));
+
+        then(actual).usingFieldByFieldElementComparator().containsExactlyInAnyOrder(johnDoe, janeDoe);
+    }
+
+    @Test
+    void shouldFindAllWithPredicateAndSort() {
+        // given
+        Person johnDoe = givenSavedPerson("John", "Doe");
+        Person johnyRoe = givenSavedPerson("Johny", "Roe");
+        Person janeDoe = givenSavedPerson("Jane", "Doe");
+
+        // when
+        Iterable<Person> actual = executor.findAll(person.lastName.eq("Doe"), Sort.by(Sort.Order.asc("firstName")));
+
+        then(actual).usingFieldByFieldElementComparator().containsExactlyInAnyOrder(janeDoe, johnDoe);
+    }
+
+    @Test
+    void shouldFindAllWithPredicateAndOrderSpecifier() {
+        // given
+        Person johnDoe = givenSavedPerson("John", "Doe");
+        Person johnyRoe = givenSavedPerson("Johny", "Roe");
+        Person janeDoe = givenSavedPerson("Jane", "Doe");
+
+        // when
+        Iterable<Person> actual = executor.findAll(person.lastName.eq("Doe"), person.firstName.asc());
+
+        then(actual).usingFieldByFieldElementComparator().containsExactlyInAnyOrder(janeDoe, johnDoe);
+    }
+
+    @Test
+    void shouldFindAllWithOrderSpecifier() {
+        // given
+        Person johnDoe = givenSavedPerson("John", "Doe");
+        Person johnyRoe = givenSavedPerson("Johny", "Roe");
+        Person janeDoe = givenSavedPerson("Jane", "Doe");
+
+        // when
+        Iterable<Person> actual = executor.findAll(person.firstName.asc());
+
+        then(actual).usingFieldByFieldElementComparator().containsExactlyInAnyOrder(janeDoe, johnDoe, johnyRoe);
+    }
+
+    @Test
+    void shouldFindAllWithPredicateAndPageable() {
+        // given
+        Person johnDoe = givenSavedPerson("John", "Doe");
+        Person johnyRoe = givenSavedPerson("Johny", "Roe");
+        Person janeDoe = givenSavedPerson("Jane", "Doe");
+
+        // when
+        Page<Person> actual = executor.findAll(person.lastName.eq("Doe"),
+                                               PageRequest.of(0, 1, Sort.by(Sort.Order.asc("firstName"))));
+
+        then(actual.getSize()).isOne();
+        then(actual.getTotalElements()).isEqualTo(2);
+        then(actual.getTotalPages()).isEqualTo(2);
+        then(actual).usingFieldByFieldElementComparator().containsExactlyInAnyOrder(janeDoe);
+    }
+
+    @Test
+    void shouldCount() {
+        // given
+        Person johnDoe = givenSavedPerson("John", "Doe");
+        Person johnyRoe = givenSavedPerson("Johny", "Roe");
+        Person janeDoe = givenSavedPerson("Jane", "Doe");
+
+        // when
+        long actual = executor.count(person.lastName.eq("Doe"));
+
+        then(actual).isEqualTo(2);
+    }
+
+    @Test
+    void shouldExist() {
+        // given
+        Person johnDoe = givenSavedPerson("John", "Doe");
+        Person johnyRoe = givenSavedPerson("Johny", "Roe");
+        Person janeDoe = givenSavedPerson("Jane", "Doe");
+
+        // when
+        boolean actual = executor.exists(person.lastName.eq("Roe"));
+
+        then(actual).isTrue();
+    }
+
+    private Person givenSavedPerson(String john, String doe) {
+        return repository.save(new Person(null, john, doe));
+    }
+}
--- infobip-spring-data-jdbc-querydsl/src/test/java/com/infobip/spring/data/jdbc/fragment/FragmentTest.java
+++ infobip-spring-data-jdbc-querydsl/src/test/java/com/infobip/spring/data/jdbc/fragment/FragmentTest.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.ResolvableType;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import static org.assertj.core.api.BDDAssertions.then;
 
@@ -29,6 +30,17 @@ public class FragmentTest extends TestBase {
         // when
         String[] actual = context.getBeanNamesForType(
                 ResolvableType.forClassWithGenerics(QuerydslJdbcFragment.class, Person.class));
+
+        // then
+        then(actual).isNotEmpty();
+    }
+
+    @Test
+    void repositoryShouldImplementQuerydslPredicateExecutor() {
+
+        // when
+        String[] actual = context.getBeanNamesForType(
+                ResolvableType.forClassWithGenerics(QuerydslPredicateExecutor.class, Person.class));
 
         // then
         then(actual).isNotEmpty();
