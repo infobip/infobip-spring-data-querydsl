e0f4901c154fb531dde9f22f57a89f709985b4c0
lpandzic <4463380+lpandzic@users.noreply.github.com>
2021-09-06 11:33:06 +0200

    removed embedded from jpa tests

--- infobip-spring-data-jpa-querydsl/src/test/java/com/infobip/spring/data/jpa/embedded/PersonWithEmbeddedFirstAndLastNameRepository.java
+++
@@ -1,8 +0,0 @@
-package com.infobip.spring.data.jpa.embedded;
-
-import com.infobip.spring.data.jpa.ExtendedQuerydslJpaRepository;
-
-public interface PersonWithEmbeddedFirstAndLastNameRepository
-        extends ExtendedQuerydslJpaRepository<PersonWithEmbeddedFirstAndLastName, Long> {
-
-}
