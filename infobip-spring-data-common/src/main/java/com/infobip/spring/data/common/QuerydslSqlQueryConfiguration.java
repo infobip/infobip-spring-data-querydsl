debb09cbb53d7ed6962216111c27e16f910ebda7
lpandzic <4463380+lpandzic@users.noreply.github.com>
2022-01-10 14:56:49 +0100

    added exception translator to QuerydslSqlQueryConfiguration

--- infobip-spring-data-common/src/main/java/com/infobip/spring/data/common/QuerydslSqlQueryConfiguration.java
+++ infobip-spring-data-common/src/main/java/com/infobip/spring/data/common/QuerydslSqlQueryConfiguration.java
@@ -1,13 +1,18 @@
 package com.infobip.spring.data.common;
 
+import javax.sql.DataSource;
+import java.util.Optional;
+
 import com.querydsl.sql.SQLQueryFactory;
 import com.querydsl.sql.SQLTemplates;
 import com.querydsl.sql.spring.SpringConnectionProvider;
+import com.querydsl.sql.spring.SpringExceptionTranslator;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.relational.core.mapping.NamingStrategy;
-
-import javax.sql.DataSource;
+import org.springframework.jdbc.support.SQLExceptionTranslator;
 
 @Import(InfobipSpringDataCommonConfiguration.class)
 @Configuration
@@ -21,8 +26,16 @@ public class QuerydslSqlQueryConfiguration {
 
     @ConditionalOnMissingBean
     @Bean
+    public SpringExceptionTranslator springExceptionTranslator(Optional<SQLExceptionTranslator> sqlExceptionTranslator) {
+        return sqlExceptionTranslator.map(SpringExceptionTranslator::new).orElseGet(SpringExceptionTranslator::new);
+    }
+
+    @ConditionalOnMissingBean
+    @Bean
     public com.querydsl.sql.Configuration querydslSqlConfiguration(SQLTemplates sqlTemplates) {
-        return new com.querydsl.sql.Configuration(sqlTemplates);
+        com.querydsl.sql.Configuration configuration = new com.querydsl.sql.Configuration(sqlTemplates);
+        configuration.setExceptionTranslator(new SpringExceptionTranslator());
+        return configuration;
     }
 
     @ConditionalOnMissingBean
@@ -31,4 +44,5 @@ public class QuerydslSqlQueryConfiguration {
                                            DataSource dataSource) {
         return new SQLQueryFactory(querydslSqlConfiguration, new SpringConnectionProvider(dataSource));
     }
+
 }
