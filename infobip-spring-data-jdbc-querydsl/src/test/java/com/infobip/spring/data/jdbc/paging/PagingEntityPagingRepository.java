44b417378b1297e884daa39ef54efe21959a941c
lpandzic <4463380+lpandzic@users.noreply.github.com>
2022-10-25 11:16:59 +0200

    added CrudRepository to PagingEntityPagingRepository

--- infobip-spring-data-jdbc-querydsl/src/test/java/com/infobip/spring/data/jdbc/paging/PagingEntityPagingRepository.java
+++ infobip-spring-data-jdbc-querydsl/src/test/java/com/infobip/spring/data/jdbc/paging/PagingEntityPagingRepository.java
@@ -1,7 +1,8 @@
 package com.infobip.spring.data.jdbc.paging;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface PagingEntityPagingRepository extends PagingAndSortingRepository<PagingEntity, Long> {
+public interface PagingEntityPagingRepository extends PagingAndSortingRepository<PagingEntity, Long>, CrudRepository<PagingEntity, Long> {
 
 }
