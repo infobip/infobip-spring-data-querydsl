180cd33a07281f9c5346a364ad40ea0c9e193569
lpandzic <4463380+lpandzic@users.noreply.github.com>
2022-10-25 11:16:10 +0200

    annotation processor fixes

--- infobip-spring-data-jdbc-annotation-processor/src/test/resources/expected/QSnakeCaseAndTransientType.java
+++ infobip-spring-data-jdbc-annotation-processor/src/test/resources/expected/QSnakeCaseAndTransientType.java
@@ -5,7 +5,7 @@ import static com.querydsl.core.types.PathMetadataFactory.*;
 import com.querydsl.core.types.dsl.*;
 
 import com.querydsl.core.types.PathMetadata;
-import jakarta.annotation.Generated;
+import javax.annotation.processing.Generated;
 import com.querydsl.core.types.Path;
 
 import com.querydsl.sql.ColumnMetadata;
