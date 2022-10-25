b3e2a5576d8b244823b3128617b1eab3a79b2c03
lpandzic <4463380+lpandzic@users.noreply.github.com>
2022-10-25 08:06:05 +0200

    fixed QuerydslR2dbcRepository

--- infobip-spring-data-r2dbc-querydsl/src/main/java/com/infobip/spring/data/r2dbc/QuerydslR2dbcRepository.java
+++ infobip-spring-data-r2dbc-querydsl/src/main/java/com/infobip/spring/data/r2dbc/QuerydslR2dbcRepository.java
@@ -1,16 +1,18 @@
 package com.infobip.spring.data.r2dbc;
 
+import java.util.Arrays;
+
 import org.springframework.data.querydsl.ReactiveQuerydslPredicateExecutor;
 import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.data.repository.reactive.ReactiveSortingRepository;
 import reactor.core.publisher.Flux;
 
-import java.util.Arrays;
-
 @NoRepositoryBean
-public interface QuerydslR2dbcRepository<T, ID> extends ReactiveSortingRepository<T, ID>, ReactiveQuerydslPredicateExecutor<T>, QuerydslR2dbcFragment<T> {
+public interface QuerydslR2dbcRepository<T, ID> extends ReactiveSortingRepository<T, ID>, ReactiveCrudRepository<T, ID>, ReactiveQuerydslPredicateExecutor<T>, QuerydslR2dbcFragment<T> {
 
     default Flux<T> save(T... entities) {
         return this.saveAll(Arrays.asList(entities));
     }
+
 }
