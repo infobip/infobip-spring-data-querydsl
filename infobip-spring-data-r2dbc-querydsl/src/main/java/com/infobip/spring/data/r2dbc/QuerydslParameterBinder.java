9ec85127be51302fcce9c17307dde6ace7b81ca3
lpandzic <lovro.pandzic@infobip.com>
2023-08-07 08:45:16 +0200

    improved type conversion in QuerydslParameterBinder

--- infobip-spring-data-r2dbc-querydsl/src/main/java/com/infobip/spring/data/r2dbc/QuerydslParameterBinder.java
+++ infobip-spring-data-r2dbc-querydsl/src/main/java/com/infobip/spring/data/r2dbc/QuerydslParameterBinder.java
@@ -5,14 +5,28 @@ import io.r2dbc.spi.R2dbcType;
 import org.springframework.r2dbc.core.DatabaseClient;
 import org.springframework.r2dbc.core.binding.BindMarkersFactory;
 
+import java.math.BigDecimal;
+import java.nio.ByteBuffer;
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 class QuerydslParameterBinder {
 
     private final BindMarkersFactory bindMarkersFactory;
+    private final Map<Class<?>, R2dbcType> classToR2dbcType;
 
     QuerydslParameterBinder(BindMarkersFactory bindMarkersFactory) {
         this.bindMarkersFactory = bindMarkersFactory;
+        var duplicateR2dbcTypes = Set.of(String.class, ByteBuffer.class, BigDecimal.class, Double.class);
+        this.classToR2dbcType = Stream.of(R2dbcType.values())
+                                      .filter(r2dbcType -> !duplicateR2dbcTypes.contains(r2dbcType.getJavaType()))
+                                      .collect(Collectors.toMap(R2dbcType::getJavaType, Function.identity()));
+        this.classToR2dbcType.put(String.class, R2dbcType.NVARCHAR);
+        this.classToR2dbcType.put(ByteBuffer.class, R2dbcType.VARBINARY);
+        this.classToR2dbcType.put(BigDecimal.class, R2dbcType.DECIMAL);
+        this.classToR2dbcType.put(Double.class, R2dbcType.DOUBLE);
     }
 
     DatabaseClient.GenericExecuteSpec bind(DatabaseClient databaseClient, List<Object> bindings, String sql) {
@@ -21,7 +35,7 @@ class QuerydslParameterBinder {
         var spec = databaseClient.sql(sqlWithParameterNames);
         for (Map.Entry<String, Object> entry : parameterNameToParameterValue.entrySet()) {
             spec = spec.bind(entry.getKey(),
-                             Parameters.in(R2dbcType.VARCHAR, entry.getValue()));
+                             Parameters.in(classToR2dbcType.get(entry.getValue().getClass()), entry.getValue()));
         }
         return spec;
     }
