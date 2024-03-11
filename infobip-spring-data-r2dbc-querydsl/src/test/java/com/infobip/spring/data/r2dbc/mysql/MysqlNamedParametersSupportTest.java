ae89348c83c677a169e0cd5cae7baf8c6ed5346c
lpandzic <lovro.pandzic@infobip.com>
2024-03-11 15:47:43 +0100

    tests wiring

--- infobip-spring-data-r2dbc-querydsl/src/test/java/com/infobip/spring/data/r2dbc/mysql/MysqlNamedParametersSupportTest.java
+++ infobip-spring-data-r2dbc-querydsl/src/test/java/com/infobip/spring/data/r2dbc/mysql/MysqlNamedParametersSupportTest.java
@@ -1,49 +1,18 @@
 package com.infobip.spring.data.r2dbc.mysql;
 
-import com.infobip.spring.data.r2dbc.EnableQuerydslR2dbcRepositories;
-import com.querydsl.sql.MySQLTemplates;
-import com.querydsl.sql.SQLTemplates;
+import com.infobip.spring.data.r2dbc.TestBase;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.*;
-
-import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 
 @ContextConfiguration(loader = MssqlExclusionContextLoader.class)
 @ActiveProfiles("mysql")
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
-@TestInstance(PER_CLASS)
-@SpringBootTest(classes = MysqlNamedParametersSupportTest.Main.class)
-public class MysqlNamedParametersSupportTest {
+public class MysqlNamedParametersSupportTest extends TestBase {
 
     @Test
     void shouldNotFail() {
         // given
         // when
         // then
-    }
-
-    @EnableQuerydslR2dbcRepositories
-    @SpringBootApplication
-    public static class Main {
-
-        public static void main(String[] args) {
-
-            new SpringApplicationBuilder(com.infobip.spring.data.r2dbc.Main.class).run(args);
-        }
-
-        @Configuration
-        public static class TestConfiguration {
-
-            @Bean
-            public SQLTemplates sqlTemplates() {
-                return new MySQLTemplates();
-            }
-        }
     }
 }
