0309679bfbc6c47e75a9ae26d7cf1c617a127154
lpandzic <lovro.pandzic@infobip.com>
2024-03-11 15:58:16 +0100

    finished setting up test that reproduces the issue

--- infobip-spring-data-r2dbc-querydsl/src/test/resources/db/migration/mysql/V1_0_0__base_schema.sql
+++ infobip-spring-data-r2dbc-querydsl/src/test/resources/db/migration/mysql/V1_0_0__base_schema.sql
@@ -1,30 +1,30 @@
-CREATE TABLE Person (
+CREATE TABLE person (
     Id        BIGINT AUTO_INCREMENT,
     FirstName NVARCHAR(20) NOT NULL,
     LastName  NVARCHAR(50) NOT NULL,
     CONSTRAINT PK_Person PRIMARY KEY (Id)
 );
 
-CREATE TABLE PersonSettings (
+CREATE TABLE personsettings (
     Id       BIGINT AUTO_INCREMENT,
     PersonId BIGINT NOT NULL,
     CONSTRAINT PK_PersonSettings PRIMARY KEY (Id),
-    CONSTRAINT FK_PersonSettings_PersonId FOREIGN KEY (PersonId) REFERENCES Person(Id) ON DELETE CASCADE
+    CONSTRAINT FK_PersonSettings_PersonId FOREIGN KEY (PersonId) REFERENCES person(Id) ON DELETE CASCADE
 );
 
-CREATE TABLE NoArgsEntity (
+CREATE TABLE noargsentity (
     Id    BIGINT AUTO_INCREMENT,
     Value NVARCHAR(20),
     CONSTRAINT PK_NoArgsEntity PRIMARY KEY (Id)
 );
 
-CREATE TABLE TransientEntity (
+CREATE TABLE transiententity (
     Id    BIGINT AUTO_INCREMENT,
     Value NVARCHAR(20),
     CONSTRAINT PK_TransientEntity PRIMARY KEY (Id)
 );
 
-CREATE TABLE PagingEntity (
+CREATE TABLE pagingentity (
     Id    BIGINT AUTO_INCREMENT,
     Value NVARCHAR(20),
     CONSTRAINT PK_PagingEntity PRIMARY KEY (Id)
